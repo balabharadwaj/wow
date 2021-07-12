576dc09005ad68962ea3e881215b2901d737bf93
balabharadwaj <61039266+balabharadwaj@users.noreply.github.com>
2021-07-12 15:35:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function dis(val) {
+    document.getElementById("result").value += val;
+}
+
+function solve() {
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clr() {
+    document.getElementById("result").value = "";
+}
+
+function backspace() {
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+}
